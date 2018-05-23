f3b2907799db79f134b72b72ce6c6f97de1f5b17
Luis Olivetti <luuisf92@gmail.com>
2018-05-23 19:57:55 -0300

    remover arquivos

--- conexao.php
+++ conexao.php
@@ -1,20 +1,13 @@
 <?php
-# Informa qual o conjunto de caracteres será usado.
-header('Content-Type: text/html; charset=utf-8');
-//definindo a conexao do sistema
-$servidor = '127.0.0.1';
-$usuarios = 'root';
-$senha = '';
-$db = 'cadefoodtruck';
-//conecta com mysql
-$connection = mysqli_connect($servidor, $usuarios, $senha, $db);
-if (!$connection) {
-  die('Não conectou ao banco : ' . mysql_error());
-}
+	header('Content-Type: text/html; charset=utf-8');
 
-//if ($resultados) {
-	//while ($row = mysqli_fetch_array($resultados)) {
-//		echo '<p> name ' . $row['name'] . '</p>';		
-//	}
-//}
+	$servidor = '127.0.0.1';
+	$usuarios = 'root';
+	$senha = '';
+	$db = 'cadefoodtruck';
+
+	$connection = mysqli_connect($servidor, $usuarios, $senha, $db);
+	if (!$connection) {
+	  die('Não conectou ao banco : ' . mysql_error());
+	}
 ?>
